3944ed837ac50c591b459b8ddb0ee22fd2919eaa
Óscar González Fresno <oscargonzalezfresno@gmail.com>
2024-10-03 00:19:09 +0200

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://oscgf.github.io',
+  base: '/portfolio',
+  integrations: [tailwind()]
+});
